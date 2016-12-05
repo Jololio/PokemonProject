305b0fc76ab2344f2f519bdf7a93ba4b14cc42bc
Jololio <jala9132@740s-pl-120.local>
2016-12-05 13:46:56 -0700

    Added To code (Errors exist)

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,5 +1,9 @@
 package pokemon.controller;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 import pokemon.model.Pokemon;
 import pokemon.view.PokemonFrame;
 import pokemon.view.PokemonPanel;
@@ -10,23 +14,25 @@ public class PokemonController
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public PokemonController()
+	{
+		pokedex = new ArrayList<Pokemon>();
+		buildPokedex();
+		
+		baseFrame = new PokemonFrame(this);
+	}
+		
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(baseFrame, "Welcome to Pokemon Inheritance");
 	}
 	
+	private void buildPokedex()
+	{
+		pokedex.add(new Geodude("derpy"));
+		pokedex.add(new Onix("Pranked"));
+		pokedex.add(new Geodude("Rock n' Roll"));
+		pokedex.add(new Voltorb("Shocking, I know"));
+	}
 	
 }
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,8 +1,11 @@
 package pokemon.view;
 
 import pokemon.controller.PokemonController;
+
 import javax.swing.*;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -35,6 +38,7 @@ public class PokemonPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		advancedArea = new JTextArea();
+		this.pokemonIcon = new ImageIcon(getClass().getResource("/poke/view/"));
 		healthField = new JTextField();
 		updateButton = new JButton();
 		pokemonIcon = new ImageIcon();
@@ -70,6 +74,17 @@ public class PokemonPanel extends JPanel
 	{
 		this.setLayout(baseLayout);
 		this.setBackground(Color.LIGHT_GRAY);
+		this.setPreferredSize(new Dimension(900, 600));
+		
+		numberField.setEditable(false);
+		numberField.setBackground(Color.GRAY);
+		advancedArea.setEditable(false);
+		advancedArea.setWrapStyleWord(true);
+		advancedArea.setLineWrap(true);
+		
+		pokemonLabel.setVerticalTextPosition(JLabel.BOTTOM);
+		pokemonLabel.setHorizontalTextPosition(JLabel.CENTER);
+		
 		this.add(advancedArea);
 		this.add(healthField);
 		this.add(updateButton);
@@ -89,12 +104,94 @@ public class PokemonPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.WEST, advancedArea, -198, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, advancedArea, -37, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, advancedArea, 114, SpringLayout.SOUTH, healthField);
+		baseLayout.putConstraint(SpringLayout.EAST, advancedArea, 0, SpringLayout.EAST, healthField);
+		baseLayout.putConstraint(SpringLayout.NORTH, healthField, 44, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, healthField, -47, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, healthField, -10, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, updateButton, 6, SpringLayout.SOUTH, advancedArea);
+		baseLayout.putConstraint(SpringLayout.EAST, updateButton, -60, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, combatField, 10, SpringLayout.SOUTH, healthField);
+		baseLayout.putConstraint(SpringLayout.WEST, combatField, -37, SpringLayout.EAST, advancedArea);
+		baseLayout.putConstraint(SpringLayout.EAST, combatField, 0, SpringLayout.EAST, advancedArea);
 	}
 	
 	private void setupListeners()
 	{
+		pokedexSelector.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent selection)
+			{
+				int selected = pokedexSelector.getSelectedIndex();
+				nameField.setText(baseController.getPokedex().get(selected).getName());
+				numberField.setText(baseController.getPokedex().get(selected).getNumber() + "");
+				combatField.setText(baseController.getPokedex().get(selected).getAttackPoints() + "");
+				speedField.setText(baseController.getPokedex().get(selected).getSpeed() + "");
+				healthField.setText(baseController.getPokedex().get(selected).getHealthPoints() + "");
+				advancedArea.setText(baseController.getPokedex().get(selected).getPokemonInformation() + "\n\n" + baseController.getPokedex().get(selected).getPokemonTypes());
+				changeColorBasedOnData(baseController.getPokedex().get(selected).getPokemonTypes());
+				changeImageDisplay(baseController.getPokedex().get(selected).getClass().getSimpleName());
+			}
+		});
 		
+		updateButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent selection)
+			{
+				if(isValidName(nameField.getText()) && isValidInteger(combatField.getText()) && isValidInteger(healthField.getText()))
+				{
+					int selected = pokedexSelector.getSelectedIndex();
+					baseController.getPokedex().get(selected).setName(nameField.getText());
+					baseController.getPokedex().get(selected).setHealthPoints(Integer.parseInt(healthField.getText()));
+					baseController.getPokedex().get(selected).setAttackPoints(Integer.parseInt(combatField.getText()));
+					baseController.getPokedex().get(selected).setSpeed(Double.parseDouble(speedField.getText()));
+				}
+			}
+		});
 	}
 	
+	private void changeColorBasedOnData(String data)
+	{
+		if (data.contains("Electric"))
+		{
+			this.setBackground(Color.YELLOW);
+		}
+		else if (data.contains("Fire"))
+		{
+			this.setBackground(Color.RED);
+		}
+		else if (data.contains("Ground"))
+		{
+			this.setBackground(Color.ORANGE);
+		}
+		else if (data.contains("Rock"))
+		{
+			this.setBackground(Color.LIGHT_GRAY);
+		}
+		else
+		{
+			this.setBackground(Color.MAGENTA);
+		}
+		
+		repaint();
+	}
+	
+	private void changeImageDisplay(String name)
+	{
+		String path = "/poke/view/images/";
+		String defaultName = "ultraball";
+		String extension = ".png";
+		try
+		{
+			pokemonIcon = new ImageIcon(getClass().getResource(path + name + extension));
+		}
+		catch (NullPointerException missingFile)
+		{
+			pokemonIcon = new ImageIcon(getClass().getResource(path + defaultName + extension));
+		}
+		pokemonLabel.setIcon(pokemonIcon);
+		repaint();
+	}
 }
